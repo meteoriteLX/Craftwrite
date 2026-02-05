d391a2c98106b644fa1f2c94e95684aa5caee122
meteoriteLX <15021762+meteoritelx@user.noreply.gitee.com>
2026-02-05 16:13:15 +0800

    basic

---
+++ Craftwrite/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- 这个div是根容器 -->
+  <div id="app">
+    <!-- 这里会显示当前路由对应的组件 -->
+    <router-view />
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+/*全局样式*/
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+
+body {
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, sans-serif;
+  line-height: 1.6;
+}
+
+/* #app {
+  min-height: 100vh; 
+} */
+</style>
---
+++ Craftwrite/src/views/HomePage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="homeLayout">
+    <div class="header">
+      <h1>Craftwrite</h1>
+    </div>
+    <div class="navigation">
+
+    </div>
+  </div>
+</template>
+ 
+<script setup>
+ 
+</script>
+ 
+<style scoped>
+
+.homeLayout{
+  background-color: #c1d1d8;
+}
+.header{
+  background-color: #94c7d1;
+}
+ 
+</style>
